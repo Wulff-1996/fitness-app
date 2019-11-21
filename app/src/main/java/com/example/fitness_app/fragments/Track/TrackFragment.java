1d9a40aa8553b814f9a6e5dc940fbe231c0a9da9
Jakob Wulff <wulff@nyxapp.net>
2019-11-21 11:45:31 +0100

    created fragment for categories for tracking

--- app/src/main/java/com/example/fitness_app/fragments/Track/TrackFragment.java
+++ app/src/main/java/com/example/fitness_app/fragments/Track/TrackFragment.java
@@ -4,10 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.fitness_app.R;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.jjoe64.graphview.series.Series;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class TrackFragment extends Fragment {
 
@@ -20,6 +34,42 @@ public class TrackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track, container, false);
+        View view = inflater.inflate(R.layout.fragment_track, container, false);
+
+        initGraphView(view);
+
+        return view;
+    }
+
+    private void initGraphView(View view){
+        GraphView graph = view.findViewById(R.id.fragment_track_graph_view);
+
+        // remove grid
+        graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);
+
+        LineGraphSeries<DataPoint> lines = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(1, 35),
+                new DataPoint(2, 42),
+                new DataPoint(3, 30),
+                new DataPoint(4, 60),
+                new DataPoint(5, 70)
+        });
+        PointsGraphSeries<DataPoint> points = new PointsGraphSeries<>(new DataPoint[] {
+                new DataPoint(1, 35),
+                new DataPoint(2, 42),
+                new DataPoint(3, 30),
+                new DataPoint(4, 60),
+                new DataPoint(5, 70)
+        });
+        points.setOnDataPointTapListener(new OnDataPointTapListener(){
+
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(getActivity(), "y: " + dataPoint.getY(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        graph.addSeries(lines);
+        graph.addSeries(points);
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
     }
 }
